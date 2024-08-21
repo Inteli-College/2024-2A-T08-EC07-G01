ce27365e9db76cbe2c1589dd282c8aede7f87f49
Antonio Artimonte <antonio.guimaraes@sou.inteli.edu.br>
2024-08-21 16:32:21 -0300

    fix: arrumei as cores da pag inicial

--- src/app/pages/index.vue
+++ src/app/pages/index.vue
@@ -13,14 +13,14 @@ definePageMeta({
     <Logo width="w-80" />
     
     <!-- Container com botões -->
-    <div class="relative border border-gray-800 rounded-lg bg-gray-200 w-[300px] pt-10">
-      <h1 class="absolute -top-5 left-1/2 transform -translate-x-1/2 px-2 bg-gray-200 text-3xl font-bold">
+    <div class="relative border border-gray-800 rounded-lg bg-white w-[300px] pt-10">
+      <h1 class="absolute -top-5 left-1/2 transform -translate-x-1/2 bg-white px-2 text-3xl font-bold">
         Análise
       </h1>
 
       <div class="flex flex-col space-y-4 p-8">
-        <Button class="w-full h-20 bg-gray-300">Histórico</Button>
-        <Button class="w-full h-20 bg-gray-300">Dashboard</Button>
+        <Button class="w-full h-20">Histórico</Button>
+        <Button class="w-full h-20">Dashboard</Button>
       </div>
 
       <Button class="absolute -left-40 top-1/2 transform -translate-y-1/2">
