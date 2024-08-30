69ba122453625388b441f7d4bafdde06119a5326
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-08-30 10:16:38 -0300

    feat: index buttons redirection

--- src/app/pages/index.vue
+++ src/app/pages/index.vue
@@ -1,25 +1,26 @@
 <script setup>
 definePageMeta({
-  layout: 'empty' 
+    layout: 'empty'
 });
 
 </script>
 
 <!-- Tela inicial -->
 <template>
-  <div class="flex justify-between w-full h-screen">
+    <div class="flex justify-between w-full h-screen">
 
-    <div class="flex flex-col justify-center w-1/2 bg-customBlue h-full items-center">
-      <LogoLand width="w-80"/>
-      <h1 class="text-white text-4xl font-bold">Cross The Line</h1>
-    </div>
-    
+        <div class="flex flex-col justify-center w-1/2 bg-customBlue h-full items-center">
+            <LogoLand width="w-80" />
+            <h1 class="text-white text-4xl font-bold">Cross The Line</h1>
+        </div>
 
-    <div class="flex flex-col justify-center w-1/2 items-center space-y-16">
-      <LandingPageButton iconName="mdi:chart-timeline-variant-shimmer" label="Previsão Diária" bgColor="bg-customGreen"/>
-      <LandingPageButton iconName="mdi:clipboard-text-outline" label="Histórico" />
-      <LandingPageButton iconName="mdi:view-dashboard-outline" label="Dashboard" />
-      <LandingPageButton iconName="mdi:progress-download" label="Treinamento Mensal" />
+
+        <div class="flex flex-col justify-center w-1/2 items-center space-y-16">
+            <LandingPageButton iconName="mdi:chart-timeline-variant-shimmer" label="Previsão Diária"
+                bgColor="bg-customGreen" redirect="/" />
+            <LandingPageButton iconName="mdi:clipboard-text-outline" label="Histórico" redirect="/history" />
+            <LandingPageButton iconName="mdi:view-dashboard-outline" label="Dashboard" redirect="/" />
+            <LandingPageButton iconName="mdi:progress-download" label="Treinamento Mensal" redirect="/" />
+        </div>
     </div>
-  </div>
 </template>
