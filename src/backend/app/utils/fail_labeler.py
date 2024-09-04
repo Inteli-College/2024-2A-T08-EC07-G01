2de77604490f57f5f7934542ed679d704f8921e9
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-09-04 11:52:54 -0300

    fix: no fail code

--- src/backend/app/utils/fail_labeler.py
+++ src/backend/app/utils/fail_labeler.py
@@ -1,6 +1,7 @@
 from app.models.KNR import KNR
 
 fails = {
+    0: "Sem falha",
     1: "Parte Traseira",
     2: "Parte Central",
     4: "Portas",
@@ -14,15 +15,10 @@ fails = {
 
 
 def label_knr(knr: KNR) -> KNR:
-    print('pre')
-    print(knr)
     if knr.real_fail == '':
         knr.real_fail = fails.get(knr.real_fail_code, "")
 
     if knr.predicted_fail == '':
         knr.predicted_fail = fails.get(knr.predicted_fail_code, "")
-
-    print('pos')
-    print(knr)
 
     return knr
