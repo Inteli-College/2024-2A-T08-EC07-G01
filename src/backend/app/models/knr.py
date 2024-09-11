ee302dd394be0bb81bb51f503166c36b460baee7
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-09-11 16:59:15 -0300

    fix: KNR model to store more than 1 fail

--- src/backend/app/models/knr.py
+++ src/backend/app/models/knr.py
@@ -1,74 +1,29 @@
-from typing import Optional
+from typing import List, Optional
 from pydantic import BaseModel, Field
 
 
 class KNR(BaseModel):
     KNR: str = Field(..., description="Car KNR")
 
-    NAME: Optional[str] = Field(..., description="Car NAME")
-    ID: Optional[int] = Field(..., description="Car ID")
-    STATUS: Optional[int] = Field(..., description="Car STATUS")
-    UNIT: Optional[str] = Field(..., description="Car UNIT")
-    VALUE_ID: Optional[str] = Field(..., description="Car VALUE_ID")
-    VALUE: Optional[str] = Field(..., description="Car VALUE")
-    DATA: Optional[str] = Field(..., description="Car DATA")
+    NAME: str = Field(..., description="Car NAME")
+    ID: int = Field(..., description="Car ID")
+    STATUS: int = Field(..., description="Car STATUS")
+    UNIT: str = Field(..., description="Car UNIT")
+    VALUE_ID: str = Field(..., description="Car VALUE_ID")
+    VALUE: str = Field(..., description="Car VALUE")
+    DATA: str = Field(..., description="Car DATA")
 
     timestamp: Optional[str] = Field(
         "", description="Date of the prediction in iso format"
     )
 
-    predicted_fail: Optional[str] = Field("", description="Predicted fail")
-    predicted_fail_code: Optional[int] = Field(-1, description="Predicted fail code")
-    indicated_test: Optional[str] = Field("", description="Indicated test")
-    real_fail: Optional[str] = Field("", description="Real fail")
-    real_fail_code: Optional[int] = Field(-1, description="Real fail code")
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "KNR": "string",
-                "NAME": "string",
-                "ID": 0,
-                "STATUS": 0,
-                "UNIT": "string",
-                "VALUE_ID": "string",
-                "VALUE": "string",
-                "DATA": "string",
-                "timestamp": "string",
-                "predicted_fail": "string",
-                "predicted_fail_code": 0,
-                "indicated_test": "string",
-                "real_fail": "string",
-                "real_fail_code": 0,
-            }
-        }
-
-        from typing import Optional
-
-
-from pydantic import BaseModel, Field
-
-
-class KNR(BaseModel):
-    KNR: str = Field(..., description="Car KNR")
-
-    NAME: Optional[str] = Field(..., description="Car NAME")
-    ID: Optional[int] = Field(..., description="Car ID")
-    STATUS: Optional[int] = Field(..., description="Car STATUS")
-    UNIT: Optional[str] = Field(..., description="Car UNIT")
-    VALUE_ID: Optional[str] = Field(..., description="Car VALUE_ID")
-    VALUE: Optional[str] = Field(..., description="Car VALUE")
-    DATA: Optional[str] = Field(..., description="Car DATA")
-
-    timestamp: Optional[str] = Field(
-        "", description="Date of the prediction in iso format"
+    predicted_fail: Optional[List[str]] = Field("", description="Predicted fail")
+    predicted_fail_code: Optional[List[int]] = Field(
+        -1, description="Predicted fail code"
     )
-
-    predicted_fail: Optional[str] = Field("", description="Predicted fail")
-    predicted_fail_code: Optional[int] = Field(-1, description="Predicted fail code")
-    indicated_test: Optional[str] = Field("", description="Indicated test")
-    real_fail: Optional[str] = Field("", description="Real fail")
-    real_fail_code: Optional[int] = Field(-1, description="Real fail code")
+    indicated_test: Optional[List[str]] = Field("", description="Indicated test")
+    real_fail: Optional[List[str]] = Field("", description="Real fail")
+    real_fail_code: Optional[List[int]] = Field(-1, description="Real fail code")
 
     class Config:
         json_schema_extra = {
