27d18122d2a4a58921c305d8b0ac3e6644696044
ItsVasconcellos <fernando.asv@uol.com.br>
2024-09-16 00:28:35 -0300

    fix: wrongful status displaying

--- src/health-check/components/Health.vue
+++ src/health-check/components/Health.vue
@@ -8,7 +8,7 @@
         <span class="font-medium mr-8 text-gray-700">FrontEnd</span>
         <span
           :class="{ 'text-red-500': frontendStatus !== 'Status: 200', 'text-green-500': frontendStatus === 'Status: 200' }"
-          class="font-semibold">{{ frontendStatus === 'Status: 200' ? 'Status: OK' : frontendStatus}}</span>
+          class="font-semibold">{{ frontendStatus === 'Status: 200' ? 'Status: OK' : frontendStatus }}</span>
       </li>
       <li class="flex justify-between p-4 bg-white shadow-md rounded-lg">
         <span class="font-medium mr-8 text-gray-700">BackEnd</span>
@@ -19,8 +19,8 @@
       <li class="flex justify-between p-4 bg-white shadow-md rounded-lg">
         <span class="font-medium mr-8 text-gray-700">MongoDB</span>
         <span
-          :class="{ 'text-red-500': databaseStatus !== 'Status: 200','text-green-500': databaseStatus === 'Status: 200' }"
-          class="font-semibold">{{ databaseStatus === 'Status: 200' ? 'Status: Error' : databaseStatus }}</span>
+          :class="{ 'text-red-500': databaseStatus !== 'Status: 200', 'text-green-500': databaseStatus === 'Status: 200' }"
+          class="font-semibold">{{ databaseStatus === 'Status: 200' ? 'Status: OK' : databaseStatus }}</span>
       </li>
     </ul>
   </div>
@@ -48,7 +48,15 @@ export default {
   methods: {
     async fetchBackendStatus() {
       try {
-        const response = await fetch('http://localhost:8000/');
+        const response = await fetch('http://localhost:8000/', {
+          mode: 'cors', // This tells the browser to make a CORS request.
+          headers: {
+            'Content-Type': 'application/json', // Specify the content type, if needed
+            // Add any other custom headers you might need
+          },
+          // Include credentials if necessary (e.g., cookies)
+          credentials: 'include' // or 'same-origin' depending on your setup
+        });
         this.backendStatus = `Status: ${response.status}`;
       } catch (error) {
         console.error('Error fetching backend data:', error);
@@ -57,10 +65,19 @@ export default {
     },
     async fetchDatabaseStatus() {
       try {
-        const response = await fetch('http://localhost:27017/');
+        const response = await fetch('http://[::]:27017/', {
+          mode: 'cors', // This tells the browser to make a CORS request.
+          headers: {
+            'Content-Type': 'application/json', // Specify the content type, if needed
+            // Add any other custom headers you might need
+          },
+          // Include credentials if necessary (e.g., cookies)
+          credentials: 'include' // or 'same-origin' depending on your setup
+        });
         const statusCode = response.status;
         console.log(`Database status: ${statusCode}`);
         this.databaseStatus = `Status: ${statusCode}`;
+        console.log(this.databaseStatus);
       } catch (error) {
         console.error('Error fetching database data:', error);
         this.databaseStatus = 'Status: Error';
@@ -68,7 +85,15 @@ export default {
     },
     async fetchFrontendStatus() {
       try {
-        const response = await fetch('http://localhost:3000/');
+        const response = await fetch('http://[::]:3000/',{
+          mode: 'cors', // This tells the browser to make a CORS request.
+          headers: {
+            'Content-Type': 'application/json', // Specify the content type, if needed
+            // Add any other custom headers you might need
+          },
+          // Include credentials if necessary (e.g., cookies)
+          credentials: 'include' // or 'same-origin' depending on your setup
+        });
         console.log(response)
         this.frontendStatus = `Status: ${response.status}`;
       } catch (error) {
