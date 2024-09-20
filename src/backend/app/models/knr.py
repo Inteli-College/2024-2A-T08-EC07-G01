e18cd14bcb3bd31502a73e5b8570f397d15d7ca4
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-09-20 10:09:07 -0300

    fix: Update model and knr models

--- src/backend/app/models/knr.py
+++ src/backend/app/models/knr.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 from pydantic import BaseModel, Field
 
 
@@ -13,18 +13,6 @@ class KNR(BaseModel):
     VALUE: Optional[str] = Field(..., description="Car VALUE")
     DATA: Optional[str] = Field(..., description="Car DATA")
 
-    timestamp: Optional[str] = Field(
-        "", description="Date of the prediction in iso format"
-    )
-
-    predicted_fails: Optional[List[str]] = Field([""], description="Predicted fail")
-    predicted_fail_codes: Optional[List[int]] = Field(
-        [-1], description="Predicted fail code"
-    )
-    indicated_test: Optional[List[str]] = Field([""], description="Indicated test")
-    real_fails: Optional[List[str]] = Field([""], description="Real fail")
-    real_fail_codes: Optional[List[int]] = Field([-1], description="Real fail code")
-
     class Config:
         json_schema_extra = {
             "example": {
@@ -36,38 +24,5 @@ class KNR(BaseModel):
                 "VALUE_ID": "Value001",
                 "VALUE": "200",
                 "DATA": "2024-01-01T12:00:00",
-                "timestamp": "2024-09-10T15:30:00",
-                "predicted_fails": ["fail1", "fail2"],
-                "predicted_fail_code": [1, 2],
-                "indicated_test": ["test1", "test2"],
-                "real_fail": ["fail1", "fail2"],
-                "real_fail_code": [1, 2],
             }
         }
-
-
-class KNRUpdate(BaseModel):
-    KNR: Optional[str] = Field(None, description="Car KNR")
-
-    NAME: Optional[str] = Field(None, description="Car NAME")
-    ID: Optional[int] = Field(None, description="Car ID")
-    STATUS: Optional[int] = Field(None, description="Car STATUS")
-    UNIT: Optional[str] = Field(None, description="Car UNIT")
-    VALUE_ID: Optional[str] = Field(None, description="Car VALUE_ID")
-    VALUE: Optional[str] = Field(None, description="Car VALUE")
-    DATA: Optional[str] = Field(None, description="Car DATA")
-
-    timestamp: Optional[str] = Field(
-        None, description="Date of the prediction in iso format"
-    )
-
-    predicted_fails: Optional[List[str]] = Field("", description="Predicted fail")
-    predicted_fail_codes: Optional[List[int]] = Field(
-        -1, description="Predicted fail code"
-    )
-    indicated_tests: Optional[List[str]] = Field("", description="Indicated test")
-    real_fails: Optional[List[str]] = Field("", description="Real fail")
-    real_fail_codes: Optional[List[int]] = Field(-1, description="Real fail code")
-
-    class Config:
-        json_schema_extra = {"example": {"real_fail": ["fail1", "fail2"]}}
