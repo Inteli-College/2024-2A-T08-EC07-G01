8ba41adad2e7cabf4de51e5b7383686da752e625
ItsVasconcellos <fernando.asv@uol.com.br>
2024-09-27 09:04:26 -0300

    feat: monguinho fix

--- src/app/components/Charts/AreaData.ts
+++ src/app/components/Charts/AreaData.ts
@@ -7,7 +7,7 @@ export interface testAreaChart {
 }
 
 // lineData.ts
-export const getAreaChart = () => {
+export function getAreaChart(): testAreaChart[] {
   return [
     { name: 'Jan', total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
     { name: 'Feb', total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
@@ -17,7 +17,7 @@ export const getAreaChart = () => {
     { name: 'Jun', total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
     { name: 'Jul', total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
   ];
-};
+}
 
 
 
