0383796b011b1f3f7a47f8435767186893be5bb9
ItsVasconcellos <fernando.asv@uol.com.br>
2024-09-28 13:33:09 -0300

    feat: styling the knrs and adding true data

--- src/app/pages/prediction/[knr].vue
+++ src/app/pages/prediction/[knr].vue
@@ -3,28 +3,37 @@
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-50">
         <!-- Tela de resultado com falha -->
-        <div class="w-full max-w-4xl p-8 bg-white border-2 rounded-lg shadow-lg" :class="resultStyle.border">
+        <div class="w-full max-w-7xl p-8 bg-white border-2 rounded-lg shadow-lg" :class="resultStyle.border">
             <div class="flex items-center gap-4 mb-6">
-                <span class="text-xl font-semibold " :class="resultStyle.title">Carro com falha prevista</span>
+                <span class="text-2xl font-bold text-center " :class="resultStyle.title">Carro {{ styles.text }} </span>
             </div>
             <p class="text-center text-gray-700 text-xl font-medium mb-6">KNR: {{ knr }}</p>
-            <div class="grid grid-cols-9 gap-4 mt-2">
-                <template v-for="(failType, _index) in failTypes" :key="_index">
-                    <div class="border-2 border-gray-30 rounded-lg px-10 py-10 mx-1"
-                        :class="[styles[failType.status].bg, styles[failType.status].border]">
-                        <p class="font-bold" :class="styles[failType.status].text">{{ failType.title }}</p>
+            <div class="grid grid-cols-5 grid-flow-row gap-4 mt-2">
+                <template v-for="(failType, index) in failTypes" :key="index">
+                    <div :class="[
+                        'border w-40 h-24 text-center flex flex-col justify-center items-center',
+                        styles[failType.status].bg,
+                        styles[failType.status].border,
+                        styles[failType.status].text,
+                    ]">
+                        <p :class="['font-bold', styles[failType.status].text]">{{ failType.title }}</p>
+                        <p :class="['font-normal break-normal', styles[failType.status].text]">{{ failType.description
+                            }}</p>
                     </div>
                 </template>
             </div>
             <!-- Botão de Voltar -->
-            <button @click="voltar" class="mt-4 px-4 py-2 bg-teal-500 text-white rounded-lg hover:bg-teal-600">
-                Voltar
-            </button>
+            <div class="flex float-end">
+                <button @click="voltar" class="mt-4 px-4 py-2 bg-teal-500 text-white rounded-lg hover:bg-teal-600">
+                    Voltar
+                </button>
+            </div>
         </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { failTypes } from './knrsData.ts';
 
 import Axios from 'axios';
 
@@ -32,6 +41,7 @@ import Axios from 'axios';
 const knr = useRoute().params.knr;
 
 const styles = {
+    text: 'sem falha prevista',
     default: {
         border: 'border-gray-300',
         bg: '',
@@ -56,78 +66,34 @@ const styles = {
 const res = await Axios.get(`http://localhost:8000/api/predictions/details/${knr}`);
 
 const failType = res.data.predicted_fail_codes;
-console.log("Teste")
-console.log(failType)
+
+
 onMounted(() => {
     failTypes.value.forEach((fail) => {
-        console.log(fail);
         failType.forEach((type) => {
             if (fail.failType === type) {
                 fail.status = 'fail';
-                if (result.value === 'success' || result.value === 'default') {
-                    result.value = 'fail';
-                }
+                styles.text = "com falha prevista";
             }
         });
     });
-    if (result.value === 'default') {
-        result.value = 'success';
-    }
 });
 
-const result = computed(() => (failType !== 0 ? 'fail' : 'success'));
+
+const result = computed(() => (failType[0] !== -1 ? 'fail' : 'success'));
 const resultStyle = computed(() => styles[result.value]);
 const resultText = computed(() => (result.value === 'fail' ? 'Falha prevista' : 'Sem falha prevista'));
 
-const failTypes = ref([
-    {
-        failType: 1,
-        title: 'Tipo 1',
-        description: 'tempo na tela',
-        status: 'default'
-    },
-    {
-        failType: 2,
-        title: 'Tipo 2',
-        description: 'tempo na tela',
-        status: 'default'
-    },
-    {
-        failType: 3,
-        title: 'Tipo 3',
-        description: 'tempo na tela',
-        status: 'default'
-    },
-    {
-        failType: 4,
-        title: 'Tipo 4',
-        description: 'tempo na tela',
-        status: 'default'
-    },
-    {
-        failType: 5,
-        title: 'Tipo 5',
-        description: 'tempo na tela',
-        status: 'default'
-    },
-    {
-        failType: 6,
-        title: 'Tipo 6',
-        description: 'tempo na tela',
-        status: 'default'
-    },
-    {
-        failType: 7,
-        title: 'Tipo 7',
-        description: 'tempo na tela',
-        status: 'default'
-    }
-]);
+
 
 const voltar = () => {
-    if (knr.value.trim()) {
-        navigateTo(`/prediction`);
-    }
+    // Reset the variables
+    failTypes.value.forEach((fail) => {
+        fail.status = 'default';
+    });
+    styles.text = 'sem falha prevista';
+    result.value = 'default';
+    navigateTo(`/prediction`);
 };
 
 </script>
