38c851adb4f6ee6f9c44e43b1cb6a6405b4052db
Antonio Artimonte <antonio.guimaraes@sou.inteli.edu.br>
2024-09-29 18:06:22 -0300

    feat: add retrain route and service in trainrouter and trainservice

--- src/backend/app/routers/train_router.py
+++ src/backend/app/routers/train_router.py
@@ -2,12 +2,14 @@ from fastapi import APIRouter, UploadFile, File, HTTPException
 import pandas as pd
 from io import BytesIO
 from app.services.train_service import TrainServiceSingleton
+from app.models.train import Train, ModelComparison
 
 router = APIRouter(prefix="/api/train", tags=["Training"])
 
 @router.post(
     "/",
     response_description="Train a model",
+    response_model=Train
 )
 async def train_model(
     df_falhas: UploadFile = File(...),
@@ -23,3 +25,36 @@ async def train_model(
         raise HTTPException(status_code=500, detail=f"Model training failed: {str(e)}")
 
     return {"message": f"Model '{model_metadata['model_name']}' trained successfully."}
+
+@router.post(
+    "/retrain",
+    response_description="Retrain the model",
+    response_model=ModelComparison
+)
+async def retrain_model(
+    df_falhas: UploadFile = File(...),
+):
+    df_falhas_content = await df_falhas.read()
+    df_falhas = pd.read_csv(BytesIO(df_falhas_content))
+    df_resultados = pd.read_csv("/app/app/pipeline/resultados.csv")
+
+    try:
+        # Use the train service to retrain the model and get the comparison
+        comparison = TrainServiceSingleton.get_instance().retrain_model(df_resultados, df_falhas)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Model retraining failed: {str(e)}")
+
+    return comparison
+
+@router.post(
+    "/select_model",
+    response_description="Select a model to use"
+)
+async def select_model(
+    model_name: str
+):
+    try:
+        TrainServiceSingleton.get_instance().select_model(model_name)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to select model: {str(e)}")
+    return {"message": f"Model '{model_name}' is now in use."}
