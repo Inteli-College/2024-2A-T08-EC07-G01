e3aa1d970b96ed78182f84cd543959635dbfcb41
Antonio Artimonte <antonio.guimaraes@sou.inteli.edu.br>
2024-09-30 17:40:25 -0300

    fix: fix the prediciton service

--- src/backend/app/services/predictions_service.py
+++ src/backend/app/services/predictions_service.py
@@ -1,7 +1,12 @@
 from app.models.predictions import Prediction, PredictionUpdate
 from app.models.knr import KNR
+from app.models.predictions import Prediction
 from app.repositories.predictions_repo import PredictionsRepository
 from typing import Optional, List
+from app.pipeline.orchestrator import Orchestrator
+import pandas as pd
+import os
+import json
 
 
 # TODO: Completar predict Service
@@ -16,8 +21,39 @@ class PredictionService:
         return self.predict_repo.get_prediction(knr)
 
     def predict(self, knr: KNR) -> Prediction:
-        # TODO: call the predictions function
-        return Prediction(knr.KNR)
+        df_input = pd.DataFrame([knr.dict()])
+
+        pipeline_file_path = os.path.join(os.getcwd(), 'app', 'pipeline', 'pipeline_principal.json')
+        with open(pipeline_file_path, "r") as file:
+            pipeline_config = json.load(file)
+
+        steps = pipeline_config.get("predict_steps", [])
+
+        dataframes = {
+        "df_input": df_input
+        }
+
+        orchestrator = Orchestrator(
+            pipeline_steps=steps,
+            dataframes=dataframes,
+            mongo_uri="mongodb://db:27017",
+            db_name="cross_the_line"
+        )
+
+        # Run the prediction pipeline
+        orchestrator.run_dynamic_pipeline()
+
+        # Get the prediction result
+        prediction = orchestrator.dataframes.get("prediction_result")
+
+        response = Prediction(
+        KNR= knr.KNR,
+        predicted_fail_codes = [prediction],
+        real_fail_codes = [-1], 
+        indicated_tests = [""]  
+        )
+        # Return the prediction
+        return response
 
     def update_prediction(self, knr: str, prediction: PredictionUpdate) -> bool:
         return self.predict_repo.update_prediction(knr, prediction)
