e12950764dddcc9c9bb0703059c9624855569351
ItsVasconcellos <fernando.asv@uol.com.br>
2024-10-09 10:32:51 -0300

    fix: stop using client

--- src/app/pages/chooseModel.vue
+++ src/app/pages/chooseModel.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-center items-center mt-10">
     <h1 class="text-center mt-10 mb-12 font-semibold text-4xl">Escolha dos Modelos</h1>
-    
+
     <div class="space-y-4 w-full flex flex-col max-w-4xl">
       <div v-for="(modelClass, index) in modelClasses" :key="index"
         class="flex items-center space-x-2 bg-customBlue rounded-lg px-4 py-3 cursor-pointer transition-colors duration-300 hover:bg-customGreen  hover:text-customGreen"
@@ -13,14 +13,13 @@
         </p>
       </div>
     </div>
-    
+
     <choose-model-modal v-if="isModalOpen" :selectedModel="selectedModel" @model-selected="updateSelectedModel"
-    @close="closeModal" />
+      @close="closeModal" />
   </div>
 </template>
 
 <script>
-'use client'
 import ChooseModelModal from '@/components/Modal/chooseModelModal.vue';
 
 export default {
