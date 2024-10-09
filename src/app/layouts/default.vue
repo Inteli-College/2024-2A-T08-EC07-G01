994ecc54b0013f55dd20a8bc09566a4539cf0121
ItsVasconcellos <fernando.asv@uol.com.br>
2024-10-09 10:39:05 -0300

    feat: styling the header

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -13,7 +13,7 @@ import normalLogo from '@/assets/static/logo.png';
       </div>
 
       <!-- Div with the centered header buttons -->
-      <div class="ml-12 absolute left-1/2 transform -translate-x-1/2 flex items-center gap-28">
+      <div class="ml-12 absolute left-1/2 transform -translate-x-1/2 flex items-center gap-20">
         <HeaderButton iconName="mdi:chart-timeline-variant-shimmer" label="Previsão Diária" redirect="/prediction" />
         <HeaderButton iconName="mdi:clipboard-text-outline" label="Histórico" redirect="/history" />
         <HeaderButton iconName="mdi:view-dashboard-outline" label="Dashboard" redirect="/dashboard" />
