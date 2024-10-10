97239083d56969a81067acccec919941e49d76a9
Antonio Artimonte <antonio.guimaraes@sou.inteli.edu.br>
2024-10-10 14:29:30 -0300

    docs; add logo

--- documentacao/docusaurus.config.js
+++ documentacao/docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/logo.ppg',
+      image: 'img/logo.png',
       navbar: {
         title: 'Documentação Grupo 01',
         logo: {
